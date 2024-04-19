a09617e54943464d54b77f3be0d5c9ddcfdc7006
Nicky Santamaria <ndsans@gmail.com>
2024-04-20 00:12:28 +0100

    initial commit

---
+++ src/DependencyInjectionManifest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YourProject;
+
+use YourProject\HelloWorld\HelloWorldDiDefinitions;
+
+class DependencyInjectionManifest extends AbstractDependencyInjection
+{
+    public static function getDependencies(): array
+    {
+        return array_merge(
+            HelloWorldDiDefinitions::getDependencies(),
+            // Add any other dependency definitions according to php-di/php-di
+        );
+    }
+}
